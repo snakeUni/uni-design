2a51bd385f998d158b293e757caabd2232c732d6
Uni <1025687605@qq.com>
2018-12-27 10:47:44 +0800

    add setting

---
+++ scripts/env.js
@@ -0,0 +1,18 @@
+'use strict'
+
+const BABEL_ENV = process.env.BABEL_ENV
+
+module.exports = () => ({
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        modules: BABEL_ENV === 'es' ? false : 'commonjs',
+        targets: {
+          browsers: ['last 1 versions', 'Firefox ESR']
+        }
+      }
+    ],
+    "@babel/preset-typescript"
+  ]
+})
