58eeb70470e7ae801c8cb02abba303b09c6f071d
Uni <1025687605@qq.com>
2019-01-04 00:15:51 +0800

    addd grid

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,5 +1,6 @@
 import Icon from './icon'
 import Button from './button'
 import Input from './input'
+import { Row, Col } from './grid'
 
-export { Icon, Button, Input }
+export { Icon, Button, Input, Row, Col }
