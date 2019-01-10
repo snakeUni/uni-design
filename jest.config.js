8d27ce169b4dc59cab2ac51ca89cc606cb294e22
Uni <1025687605@qq.com>
2019-01-10 21:19:54 +0800

    change setting

--- jest.config.js
+++ jest.config.js
@@ -1,31 +1,11 @@
-
-const transformIgnorePatterns = [
-  '/dist/',
-  'node_modules/[^/]+?/(?!(es|node_modules)/)', // Ignore modules without es dir
-];
-
 module.exports = {
-  verbose: true,
-  setupFiles: ['./tests/setup.js'],
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'md'],
-  modulePathIgnorePatterns: ['/_site/'],
-  testPathIgnorePatterns: ['/node_modules/', 'node'],
   transform: {
-    '\\.tsx?$': './node_modules/antd-tools/lib/jest/codePreprocessor',
-    '\\.js$': './node_modules/antd-tools/lib/jest/codePreprocessor',
-    '\\.md$': './node_modules/antd-tools/lib/jest/demoPreprocessor',
+    '.(ts|tsx)': 'ts-jest',
   },
-  testRegex: `${libDir === 'dist' ? 'demo' : '.*'}\\.test\\.js$`,
-  collectCoverageFrom: [
-    'components/**/*.{ts,tsx}',
-    '!components/*/__tests__/**/type.tsx',
-    '!components/**/*/interface.{ts,tsx}',
-  ],
-  transformIgnorePatterns,
-  globals: {
-    'ts-jest': {
-      tsConfig: './tsconfig.test.json',
-    },
+  moduleNameMapper: {
+    "\\.(css|less)$": "identity-obj-proxy"
   },
-  testURL: 'http://localhost',
+  testPathIgnorePatterns: ['/node_modules/', 'node', '/es/', '/lib/'],
+  testRegex: '(/test/.*|\\.(test|spec))\\.(ts|tsx|js)$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'json'],
 };
