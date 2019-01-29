3d381dddc21b26d1375dc59c5f1998af63a7d9f9
Uni <1025687605@qq.com>
2019-01-29 22:21:25 +0800

    add collapse

--- src/components/message/message.tsx
+++ src/components/message/message.tsx
@@ -1,10 +1,7 @@
 import * as React from 'react'
-import * as ReactDOM from 'react-dom'
 import { InstanceProps } from './interface'
-import { getUidString } from '../utils'
 // import Toast from './toast'
 
-const { useState, useEffect } = React
 const noop = () => {}
 
 const defaultProps: InstanceProps = {
@@ -15,20 +12,10 @@ const defaultProps: InstanceProps = {
   onClose: noop
 }
 
-const addMessage = (props: InstanceProps) => {
-  const id = getUidString()
-}
-
 const Message: React.FC<InstanceProps> & { defaultProps: Partial<InstanceProps> } = props => {
   const { children } = props
-  const [visible, setVisible] = useState(true)
-  useEffect(() => {
-    if (!children) {
-      setVisible(false)
-    }
-    return () => {}
-  })
-  return <div className="uni-message-group" />
+
+  return <div className="uni-message-group">{children}</div>
 }
 
 Message.defaultProps = defaultProps
